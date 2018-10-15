09a1b91aa4bf6b387e3b64fc3d6bc66ed4a5f043
Sean Parker <sean.parker067@gmail.com>
2018-10-15 20:53:30 +0100

    Remove unsupported mysql column type

--- back-end/src/db/entity/User.ts
+++ back-end/src/db/entity/User.ts
@@ -11,7 +11,7 @@ export class User {
   @Column("varchar", { length: 255 })
   email: string;
 
-  @Column("boolean")
+  @Column()
   emailVerified: boolean;
 
   @Column("varchar", { length: 255 })
