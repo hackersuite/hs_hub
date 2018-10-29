d7704018ef9228b05338e71bf1438e144303f1ea
Kristijonas <kzalys@gmail.com>
2018-10-29 17:33:13 +0000

    Update AchievementProgress DB model to have a composite primary key

--- back-end/src/db/entity/hub/achievementProgress.ts
+++ back-end/src/db/entity/hub/achievementProgress.ts
@@ -1,13 +1,13 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+import { Entity, ManyToOne, PrimaryColumn } from "typeorm";
 import { User } from "./";
 
 @Entity()
 export class AchievementProgress {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
+  @PrimaryColumn()
   achievementId: string;
+
+  @PrimaryColumn()
+  progress: number;
 
   @ManyToOne(type => User, user => user.achievementsProgress)
   user: User;
