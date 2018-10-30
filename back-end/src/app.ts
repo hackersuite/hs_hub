120995af19d2e41ac910314dc3e85ebec45becad
Hossein.miraftabi <seyedmohammadhossein.miraftabi@student.manchester.ac.uk>
2018-10-30 15:23:00 +0000

    Implement event model for the DB

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -128,7 +128,8 @@ const createDatabaseOptions = (): ConnectionOptions[] => {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: [
-      __dirname + "/db/entity/user{.js,.ts}"
+      __dirname + "/db/entity/user{.js,.ts}",
+      __dirname + "/db/entity/event{.js, .ts}"
     ],
     // Per TypeOrm documentation, this is unsafe for production
     // We should instead use migrations to change the database
