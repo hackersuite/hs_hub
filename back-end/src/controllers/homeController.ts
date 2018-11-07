09934f5ddc9564db0a95d8f1595beb3b26c7b828
Kristijonas <kzalys@gmail.com>
2018-11-07 17:18:03 +0000

    Finish merger

--- back-end/src/controllers/homeController.ts
+++ back-end/src/controllers/homeController.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Request, Response, NextFunction } from "express";
 import { getAllReservations, getAllHardwareItems } from "../util/hardwareLibrary";
 
@@ -15,29 +14,6 @@ export class HomeController {
     res.render("pages/hardware", { items });
   }
 
-  public login(req: Request, res: Response, next: NextFunction) {
-    res.render("pages/login");
-  }
-
-  public async admin(req: Request, res: Response, next: NextFunction) {
-    try {
-      const reservations = await getAllReservations();
-      res.render("pages/admin", { reservations: reservations || [] });
-    } catch (err) {
-      return next(err);
-    }
-  }
-=======
-import { Request, Response, NextFunction } from "express";
-import { getAllReservations } from "../util/hardwareLibrary";
-
-/**
- * A controller for auth methods
- */
-export class HomeController {
-  public dashboard(req: Request, res: Response, next: NextFunction) {
-    res.render("pages/dashboard");
-  }
 
   public login(req: Request, res: Response, next: NextFunction) {
     res.render("pages/login");
@@ -51,5 +27,4 @@ export class HomeController {
       return next(err);
     }
   }
->>>>>>> admin-panel
 }
