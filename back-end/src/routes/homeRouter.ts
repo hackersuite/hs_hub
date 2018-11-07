42ac54c5184b4c4012235f64f16780d132f8438a
Kristijonas <kzalys@gmail.com>
2018-11-07 17:18:38 +0000

    Fix merge conflict

--- back-end/src/routes/homeRouter.ts
+++ back-end/src/routes/homeRouter.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Router } from "express";
 import { HomeController } from "../controllers";
 import { checkIsLoggedIn, checkIsVolunteer } from "../util/user/authorization";
@@ -24,27 +23,4 @@ export const homeRouter = (): Router => {
     homeController.dashboard);
 
   return router;
-=======
-import { Router } from "express";
-import { HomeController } from "../controllers";
-import { checkIsLoggedIn, checkIsVolunteer } from "../util/user/authorization";
-
-export const homeRouter = (): Router => {
-  const router = Router();
-
-  const homeController = new HomeController();
-
-  router.get("/admin",
-    checkIsVolunteer,
-    homeController.admin);
-
-  router.get("/login",
-    homeController.login);
-
-  router.get("/",
-    checkIsLoggedIn,
-    homeController.dashboard);
-
-  return router;
->>>>>>> admin-panel
 };
