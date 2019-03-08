5821a26930e2e698c376a8ea7de2c0c0d408f29d
Kristijonas <kzalys@gmail.com>
2019-03-08 17:43:29 +0000

    Refactor the hardware list so it is rendered by front-end JS

--- src/public/js/hardware.js
+++ src/public/js/hardware.js
@@ -10,6 +10,96 @@ function closeQR() {
   $("#reservation-qr-token").html("");
 }
 
+var itemsContainerId = "items-container";
+
+var itemTemplate = '<div class="col-lg-3 col-md-6"><div class="card">'
+                 + '<div class="card-header #headerClass"><h4 class="card-title">#name</h4></div>'
+                 + '<div class="card-body item-content"><div class="item-img-container">'
+                 + '<img class="item-img" src="#imageURL" alt=""></div><div><b>In stock:</b>#stock</div>'
+                 + '#itemControls'
+                 + '</div></div></div>';
+
+var reservedItemUI = '<div><b>You have reserved:</b>#reservedQuantity. Expires in #expiresIn minutes.'
+                   + '<button type="button" class="btn btn-warning" data-dismiss="modal"'
+                   + 'onclick="openQR(#reservationToken)">QR</button>'
+                   + '<button type="button" class="btn btn-danger" data-dismiss="modal"'
+                   + 'onclick="cancelReservation(#reservationToken)">CANCEL</button>';
+
+var takenItemUI = '<div><b>You have taken:</b>#reservedQuantity.'
+                + '<button type="button" class="btn btn-warning" data-dismiss="modal"'
+                + 'onclick="openQR(#reservationToken)">QR</button>';
+
+var takeItemUI = '<div><span><b>Quantity to take:</b></span>'
+               + '<input type="number" name="quantity" id="#itemId-reservation-quantity"'
+               + 'class="number-input" placeholder="quantity" value="1"></div>'
+               + '<button type="button" class="btn btn-success" data-dismiss="modal"'
+               + 'onclick="reserve(#itemId)">TAKE</button>';
+
+var outOfStockUI = '<div><b>Sorry. This item is currently out of stock!</b></div>';
+
+var hardwareItems = [];
+var filters = { all: "all", reserved: "reserved" };
+var currentFilter = filters.all;
+
+var headerClasses = {
+  default: "card-header-success",
+  reserved: "card-header-warning",
+  taken: "card-header-danger"
+}
+
+function renderItems(items) {
+  for (var index = 0; index < items.length; index++) {
+    var item = items[index];
+    var itemString = makeItemString(item);
+    $("#" + itemsContainerId).append(itemString);
+  }
+}
+
+function makeItemString(item) {
+  var itemString = itemTemplate
+                  .replace(/#headerClass/g, getItemHeaderClass(item))
+                  .replace(/#name/g, item.itemName)
+                  .replace(/#imageURL/g, item.itemURL)
+                  .replace(/#stock/g, item.itemsLeft)
+                  .replace(/#itemControls/g, getItemControls(item));
+  return itemString;
+}
+
+function getItemControls(item) {
+  if (item.taken) {
+    return takenItemUI
+          .replace(/#reservedQuantity/g, item.reservationQuantity)
+          .replace(/#reservationToken/g, item.reservationToken);
+  } else if (item.reserved) {
+    return reservedItemUI
+          .replace(/#reservedQuantity/g, item.reservationQuantity)
+          .replace(/#expiresIn/g, item.expiresIn)
+          .replace(/#reservationToken/g, item.reservationToken);
+  } else if (item.itemsLeft == 0) {
+    return outOfStockUI;
+  } else {
+    return takeItemUI
+          .replace(/#itemId/g, item.itemId);
+  }
+}
+
+function getItemHeaderClass(item) {
+  if (item.taken)
+    return headerClasses.taken;
+  else if (item.reserved)
+    return headerClasses.reserved;
+  else
+    return headerClasses.default;
+}
+
+function setFilter(filter) {
+  currentFilter = filters[filter];
+}
+
+function search(key) {
+  
+}
+
 function reserve(itemID) {
   var quantity = $("#" + itemID + "-reservation-quantity").val();
   $.post({
