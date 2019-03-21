47647372ec0502333d2c749d4b31572545a955e6
Kristijonas <kzalys@gmail.com>
2019-03-21 12:39:42 +0000

    Improve hardware item validation

--- src/db/entity/hub/hardwareItem.ts
+++ src/db/entity/hub/hardwareItem.ts
@@ -1,16 +1,18 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { ReservedHardwareItem } from "./reservedHardwareItem";
-import { IsUrl, IsString, IsNumber } from "class-validator";
+import { IsUrl, IsString, IsNumber, IsDefined } from "class-validator";
 
 @Entity()
 export class HardwareItem {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsDefined({ message: "The name must be defined" })
   @Column("varchar", { length: 255, nullable: false, unique: true })
   @IsString()
   name: string;
 
+  @IsDefined({ message: "The total stock must be defined" })
   @Column()
   @IsNumber()
   totalStock: number;
@@ -19,12 +21,13 @@ export class HardwareItem {
   @IsNumber()
   reservedStock: number;
 
-  @Column()
   @IsNumber()
+  @Column()
   takenStock: number;
 
-  @Column("varchar", { length: 1024 })
+  @IsDefined({ message: "The item URL must be defined" })
   @IsUrl()
+  @Column("varchar", { length: 1024 })
   itemURL: string;
 
   @OneToMany(() => ReservedHardwareItem, reservedHardwareItem => reservedHardwareItem.hardwareItem)
