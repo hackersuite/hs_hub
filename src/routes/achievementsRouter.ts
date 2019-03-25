0e08001f1ad91fce5e7a095e5d9e41f00610538d
Kristijonas <kzalys@gmail.com>
2019-03-25 22:57:42 +0000

    Implement AchievementsRouter with the new architecture

--- src/routes/achievementsRouter.ts
+++ src/routes/achievementsRouter.ts
@@ -1,16 +1,27 @@
 import { Router } from "express";
-import { AchievementsController } from "../controllers/achievementsController";
+import { AchievementsController } from "../controllers/";
 import { checkIsLoggedIn, checkIsOrganizer } from "../util/user";
+import { AchievementsService, AchievementsProgressService } from "../services";
+import { LocalAchievementsRepository } from "../util/achievements/localAchievementsRepository";
+import { localAchievements } from "../util/achievements/localAchievements";
+import { getRepository } from "typeorm";
+import { AchievementProgress } from "../db/entity/hub";
 
 export const achievementsRouter = (): Router => {
-  const router = Router();
-  const achievementsController = new AchievementsController();
+  const achievementsService: AchievementsService =
+    new AchievementsService(new LocalAchievementsRepository(localAchievements));
 
-  // /**
-  //  * GET /achievements
-  //  * Returns all implemented achievements
-  //  */
-  // router.get("/", achievementsController.getAllAchievements);
+  const achievementsProgressService: AchievementsProgressService =
+    new AchievementsProgressService(getRepository(AchievementProgress), achievementsService);
+
+  const router = Router();
+  const achievementsController = new AchievementsController(achievementsService, achievementsProgressService);
+
+  /**
+   * GET /achievements
+   * Returns all implemented achievements
+   */
+  router.get("/", achievementsController.getAllAchievements);
 
   /**
    * GET /achievements/progress
@@ -18,23 +29,23 @@ export const achievementsRouter = (): Router => {
    */
   router.get("/progress", checkIsLoggedIn, achievementsController.getProgressForAllAchievements);
 
-  /**
-   * GET /achievements/:achievementId/progress
-   * Returns the user's progress on a specific achievement
-   */
-  router.get("/:achievementId/progress", checkIsLoggedIn, achievementsController.getProgressForAchievement);
+  // /**
+  //  * GET /achievements/:achievementId/progress
+  //  * Returns the user's progress on a specific achievement
+  //  */
+  // router.get("/:achievementId/progress", checkIsLoggedIn, achievementsController.getProgressForAchievement);
 
-  /**
-   * POST /achievements/:achievementId/incrementProgress
-   * Increments the user's progress on a specific achievement
-   */
-  router.post("/:achievementId/incrementProgress", checkIsLoggedIn, achievementsController.incrementProgressForAchievement);
+  // /**
+  //  * POST /achievements/:achievementId/incrementProgress
+  //  * Increments the user's progress on a specific achievement
+  //  */
+  // router.post("/:achievementId/incrementProgress", checkIsLoggedIn, achievementsController.incrementProgressForAchievement);
 
-  /**
-   * POST /achievements/:achievementId/setProgress
-   * Sets a users progress to a given value
-   */
-  router.post("/:achievementId/setProgress", checkIsOrganizer, achievementsController.setUserProgressForAchievement);
+  // /**
+  //  * POST /achievements/:achievementId/setProgress
+  //  * Sets a users progress to a given value
+  //  */
+  // router.post("/:achievementId/setProgress", checkIsOrganizer, achievementsController.setUserProgressForAchievement);
 
   return router;
 };
