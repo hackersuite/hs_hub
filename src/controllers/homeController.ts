8753a65af32d7d8efa68d9a0a92b4764074e70b0
Kristijonas <kzalys@gmail.com>
2019-04-09 21:00:48 +0300

    Fix unit tests

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { getAllReservations, getAllHardwareItems } from "../util/hardwareLibrary";
-import { Cache } from "../util/cache";
 // import { Achievements } from "../util/achievements";
-import { EventCached } from "../util/cache/models/objects";
 import { Announcement } from "../db/entity/hub";
 import { getConnection } from "typeorm";
 import { AuthLevels } from "../util/user";
@@ -12,19 +10,19 @@ import { AuthLevels } from "../util/user";
  */
 export class HomeController {
   public async dashboard(req: Request, res: Response, next: NextFunction) {
-    const events: EventCached[] = await Cache.events.getElements();
     const announcements: Announcement[] = await getConnection("hub")
       .getRepository(Announcement)
       .createQueryBuilder("announcement")
       .orderBy("announcement.createdAt", "DESC")
       .limit(5)
       .getMany();
-    res.render("pages/dashboard", { events, announcements });
+    // TODO: re-implement the list of events when the service architecture refactor is finished
+    res.render("pages/dashboard", { events: [], announcements });
   }
 
   public async challenges(req: Request, res: Response, next: NextFunction) {
-    const challenges = await Cache.challenges.getElements();
-    res.render("pages/challenges", { challenges });
+    // TODO: re-implement the list of challenges when the service architecture refactor is finished
+    res.render("pages/challenges", { challenges: [] });
   }
 
   // public async achievements(req: Request, res: Response, next: NextFunction) {
