ef39e980663f4a726ff6d111a3e23a23293efca9
Sean Parker <sean.parker067@gmail.com>
2019-04-26 23:08:19 +0100

    Use mocked cache in announcement service tests

--- test/unit/services/announcementService.test.ts
+++ test/unit/services/announcementService.test.ts
@@ -1,28 +1,41 @@
 import * as dotenv from "dotenv";
 import { createTestDatabaseConnection, closeTestDatabaseConnection, reloadTestDatabaseConnection } from "../../util/testUtils";
 import { getRepository, Repository } from "typeorm";
-import { Challenge, Announcement } from "../../../src/db/entity/hub";
+import { Announcement } from "../../../src/db/entity/hub";
 import { AnnouncementService } from "../../../src/services/announcement";
+import { Cache } from "../../../src/util/cache/cache";
+
+jest.mock("../../../src/util/cache/cache");
 
 const testAnnouncement: Announcement = new Announcement("Test Announcement");
 
 let announcementService: AnnouncementService;
+let cache: Cache;
+let mockCache: jest.Mocked<Cache>;
 
-beforeAll(async (): Promise<void> => {
+beforeAll(async (done: jest.DoneCallback): Promise<void> => {
   dotenv.config({ path: ".env" });
-
   await createTestDatabaseConnection([ Announcement ]);
-  announcementService = new AnnouncementService(getRepository(Announcement));
+
+  done();
 });
 
-beforeEach(async (): Promise<void> => {
+beforeEach(async (done: jest.DoneCallback): Promise<void> => {
   await reloadTestDatabaseConnection();
+
+  done();
 });
 
 /**
  * Announcement tests
  */
 describe("Announcement service tests", (): void => {
+  const { Cache } = jest.requireActual("../../../src/util/cache/cache");
+
+  beforeEach((): void => {
+    cache = new Cache();
+    announcementService = new AnnouncementService(getRepository(Announcement), cache);
+  });
   /**
    * Test finding the most recent announcements
    */
@@ -41,7 +54,7 @@ describe("Announcement service tests", (): void => {
     expect(recentAnnouncements).toBeDefined();
     expect(recentAnnouncements.length).toBe(RECENT_ANNOUNCEMENTS);
   });
-  test("Should return empty array with no announcements", async (): Promise<void> => {
+  test("Should return empty array with no announcements", async (): Promise<void> => { 
     // Ensure that an empty array returned with no announcements
     const recentAnnouncements: Announcement[] = await announcementService.getMostRecentAnnouncements(1);
     expect(recentAnnouncements).toBeDefined();
@@ -62,6 +75,25 @@ describe("Announcement service tests", (): void => {
     const foundAnnouncement: Announcement = await announcementRepository.findOne(testAnnouncement);
     expect(foundAnnouncement).toBeDefined();
     expect(foundAnnouncement.id).toBe(testAnnouncement.id);
+  });
+});
+
+
+describe("Announcement service cache tests", (): void => {
+  beforeEach((): void => {
+    mockCache = new Cache() as jest.Mocked<Cache>;
+    mockCache.getAll.mockImplementation((className: string): any => {
+      return [];
+    });
+
+    announcementService = new AnnouncementService(getRepository(Announcement), mockCache);
+  });
+
+
+  test("Should ensure that the cache is used in the announcement service", async (): Promise<void> => {
+    await announcementService.getMostRecentAnnouncements(1);
+    expect(mockCache.getAll.mock.calls[0][0]).toEqual(Announcement.name);
+    expect(mockCache.setAll).toBeCalled();
   });
 });
 
