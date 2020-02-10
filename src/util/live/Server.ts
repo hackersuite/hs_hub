df3dd36e3d5b9840397098570d05d80ed0a5cdcb
Amish Shah <amishshah.2k@gmail.com>
2020-02-10 22:55:16 +0000

    Match auth cookie on socket.io

--- src/util/live/Server.ts
+++ src/util/live/Server.ts
@@ -25,6 +25,13 @@ export class LiveServer implements LiveServerInterface {
         if (this._socketIO) return;
         this._socketIO = server;
         this._nsp = server.of("/hardware/live");
+        this._nsp.on("connect", socket => {
+            if (!socket.handshake.headers || typeof socket.handshake.headers.cookie !== "string") {
+                socket.disconnect();
+                return;
+            }
+            const token = socket.handshake.headers.cookie.match(/Authorization=([a-zA-Z\.\d]+)/)[1];
+        });
         console.log("  Attached socket.io server!");
     }
 
