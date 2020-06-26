3ab1ee65612e5bd0ac13cebf32abede9fd6c822e
Amish Shah <amishshah.2k@gmail.com>
2020-06-26 13:42:51 +0100

    chore: setup lint and jest config files

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,11 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/test"
-  ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  "coveragePathIgnorePatterns":[
-    "<rootDir>/dist/",
-    "<rootDir>/node_modules/",
-    "<rootDir>/test/util/"
-  ],
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-}
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/test"],
+  coveragePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/", "<rootDir>/test/util/"],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.jest.json'
+    }
+  }
+};
