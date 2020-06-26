9eafb6d07499510ab959c50a52e836078497e0ec
Amish Shah <amishshah.2k@gmail.com>
2020-06-26 20:43:53 +0100

    refactor: strict mode for challengeService

--- src/services/challenges/challengeService.ts
+++ src/services/challenges/challengeService.ts
@@ -25,7 +25,7 @@ export class ChallengeService implements ChallengeServiceInterface {
   };
 
   public findByID = async (challengeID: number): Promise<Challenge> => {
-    const challenge: Challenge = await this.challengeRepository.findOne(challengeID);
+    const challenge = await this.challengeRepository.findOne(challengeID);
     if (!challenge) {
       throw new ApiError(HttpResponseCode.BAD_REQUEST, `Could not find challenge with given id ${challengeID}`);
     }
